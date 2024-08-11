21fbbb6feb6b1ac6157665cb6c685962924848d9
serial-hugger <serialhugger13@gmail.com>
2024-08-10 22:48:50 -0400

    Problem 1 complete.

---
+++ Problem1.cs
@@ -0,0 +1,35 @@
+using System.Globalization;
+
+namespace daily_problems;
+
+/*
+Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+   
+For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+   
+Bonus: Can you do this in one pass?
+ */
+
+public class Problem1
+{
+    public bool CheckForPair(List<int> numbers, int k)
+    {
+        int i1 = 0;
+        int i2 = 0;
+        foreach (var number1 in numbers)
+        {
+            i2 = 0;
+            foreach (var number2 in numbers)
+            {
+                if (number1 + number2 == k && i1!=i2)
+                {
+                    return true;
+                }
+                i2++;
+            }
+            i1++;
+        }
+
+        return false;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using daily_problems;
+
+Console.WriteLine("Problem #1:"+new Problem1().CheckForPair(new List<int>() {10, 15, 3, 7},17));
