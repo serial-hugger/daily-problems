7aff5c901c89cbb08169ea4241770327f9d29198
serial-hugger <serialhugger13@gmail.com>
2024-08-24 00:35:04 -0400

    Made project speperate folder

---
+++ daily-problems.Tests/Problem1Test.cs
@@ -0,0 +1,17 @@
+using daily_problems;
+using JetBrains.Annotations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace daily_problems.Tests;
+
+[TestClass]
+[TestSubject(typeof(Problem1))]
+public class Problem1Test
+{
+
+    [TestMethod]
+    public void METHOD()
+    {
+        
+    }
+}
